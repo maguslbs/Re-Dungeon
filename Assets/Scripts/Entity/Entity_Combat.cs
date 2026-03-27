c141b8d598b3602d163d535672d7c7e8e31da67e
Muhammad Agus <magussyaputra28@gmail.com>
2026-03-27 23:47:40 +0700

    [Add] VFX Controller, [Add] VFX RandomOffset

--- Assets/Scripts/Entity/Entity_Combat.cs
+++ Assets/Scripts/Entity/Entity_Combat.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Entity_Combat : MonoBehaviour
 {
+    private Entity_VFX vfx;
 
     public float damage = 10;
 
@@ -10,13 +11,23 @@ public class Entity_Combat : MonoBehaviour
     [SerializeField] private float targetCheckRadius = 1;
     [SerializeField] private LayerMask whatIsTarget;
 
+    private void Awake()
+    {
+        vfx = GetComponent<Entity_VFX>();
+    }
+
     public void PerformAttack()
     {
 
         foreach (var target in GetDetectedColliders())
         {
             IDamageable damageable = target.GetComponent<IDamageable>();
-            damageable?.TakeDamage(damage, transform);
+
+            if (damageable == null)
+                continue;
+
+            damageable.TakeDamage(damage, transform);
+            vfx.CreateOnHitVFX(target.transform);
         }
     }
 
